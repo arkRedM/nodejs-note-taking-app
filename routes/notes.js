d4e931830c1c722e1d42e52b05d509bf75ae71b4
abhinav-ark <abhinav.reddy@codenicely.in>
2019-02-23 18:11:25 +0530

    added form to add notes

--- routes/notes.js
+++ routes/notes.js
@@ -2,8 +2,19 @@ var express = require('express');
 var router = express.Router();
 const path = require('path');
 
-router.get('/notes/:bro', function(req, res){
-    res.send('LOLOL ' + req.params.bro + '!')
+const NoteData = require('../models/notes');
+
+
+router.get('/notes/add', (req, res) => {
+    res.render("notes", {"title": "Add Notes"})
+});
+
+router.post('/notes/add', (req, res) => {
+    NoteData.create({
+        title: req.body.title,
+        subtitle: req.body.subtitle,
+        notes: req.body.notes
+    })
 });
 
 
